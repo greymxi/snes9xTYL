02344335c2017562a9019b1c9104ff34dc7c20b7
greymxi <greyimm@gmail.com>
2026-04-20 02:03:39 -0400

    Update getset.h

--- getset.h
+++ getset.h
@@ -1,234 +1,347 @@
-/*
- * Snes9x - Portable Super Nintendo Entertainment System (TM) emulator.
- *
- * (c) Copyright 1996 - 2001 Gary Henderson (gary.henderson@ntlworld.com) and
- *                           Jerremy Koot (jkoot@snes9x.com)
- *
- * Super FX C emulator code 
- * (c) Copyright 1997 - 1999 Ivar (ivar@snes9x.com) and
- *                           Gary Henderson.
- * Super FX assembler emulator code (c) Copyright 1998 zsKnight and _Demo_.
- *
- * DSP1 emulator code (c) Copyright 1998 Ivar, _Demo_ and Gary Henderson.
- * C4 asm and some C emulation code (c) Copyright 2000 zsKnight and _Demo_.
- * C4 C code (c) Copyright 2001 Gary Henderson (gary.henderson@ntlworld.com).
- *
- * DOS port code contains the works of other authors. See headers in
- * individual files.
- *
- * Snes9x homepage: http://www.snes9x.com
- *
- * Permission to use, copy, modify and distribute Snes9x in both binary and
- * source form, for non-commercial purposes, is hereby granted without fee,
- * providing that this license information and copyright notice appear with
- * all copies and any derived work.
- *
- * This software is provided 'as-is', without any express or implied
- * warranty. In no event shall the authors be held liable for any damages
- * arising from the use of this software.
- *
- * Snes9x is freeware for PERSONAL USE only. Commercial users should
- * seek permission of the copyright holders first. Commercial use includes
- * charging money for Snes9x or software derived from Snes9x.
- *
- * The copyright holders request that bug fixes and improvements to the code
- * should be forwarded to them so everyone can benefit from the modifications
- * in future versions.
- *
- * Super NES and Super Nintendo Entertainment System are trademarks of
- * Nintendo Co., Limited and its subsidiary companies.
- */
-#ifndef _sa1_h_
-#define _sa1_h_
+/*******************************************************************************
+  Snes9x - Portable Super Nintendo Entertainment System (TM) emulator.
+ 
+  (c) Copyright 1996 - 2002 Gary Henderson (gary.henderson@ntlworld.com) and
+                            Jerremy Koot (jkoot@snes9x.com)
 
-#include "memmap.h"
+  (c) Copyright 2001 - 2004 John Weidman (jweidman@slip.net)
 
-/*
-struct SSA1Registers {
-    uint8   PB;
-    uint8   DB;
-    pair    P;
+  (c) Copyright 2002 - 2004 Brad Jorsch (anomie@users.sourceforge.net),
+                            funkyass (funkyass@spam.shaw.ca),
+                            Joel Yliluoma (http://iki.fi/bisqwit/)
+                            Kris Bleakley (codeviolation@hotmail.com),
+                            Matthew Kendora,
+                            Nach (n-a-c-h@users.sourceforge.net),
+                            Peter Bortas (peter@bortas.org) and
+                            zones (kasumitokoduck@yahoo.com)
 
-    pair    A;
-    pair    D;
+  C4 x86 assembler and some C emulation code
+  (c) Copyright 2000 - 2003 zsKnight (zsknight@zsnes.com),
+                            _Demo_ (_demo_@zsnes.com), and Nach
 
-    pair    S;
-    pair    X;
+  C4 C++ code
+  (c) Copyright 2003 Brad Jorsch
 
-    pair    Y;
-    uint16  PC;
-};
-*/
-struct SSA1Registers
+  DSP-1 emulator code
+  (c) Copyright 1998 - 2004 Ivar (ivar@snes9x.com), _Demo_, Gary Henderson,
+                            John Weidman, neviksti (neviksti@hotmail.com),
+                            Kris Bleakley, Andreas Naive
+
+  DSP-2 emulator code
+  (c) Copyright 2003 Kris Bleakley, John Weidman, neviksti, Matthew Kendora, and
+                     Lord Nightmare (lord_nightmare@users.sourceforge.net
+
+  OBC1 emulator code
+  (c) Copyright 2001 - 2004 zsKnight, pagefault (pagefault@zsnes.com) and
+                            Kris Bleakley
+  Ported from x86 assembler to C by sanmaiwashi
+
+  SPC7110 and RTC C++ emulator code
+  (c) Copyright 2002 Matthew Kendora with research by
+                     zsKnight, John Weidman, and Dark Force
+
+  S-DD1 C emulator code
+  (c) Copyright 2003 Brad Jorsch with research by
+                     Andreas Naive and John Weidman
+ 
+  S-RTC C emulator code
+  (c) Copyright 2001 John Weidman
+  
+  ST010 C++ emulator code
+  (c) Copyright 2003 Feather, Kris Bleakley, John Weidman and Matthew Kendora
+
+  Super FX x86 assembler emulator code 
+  (c) Copyright 1998 - 2003 zsKnight, _Demo_, and pagefault 
+
+  Super FX C emulator code 
+  (c) Copyright 1997 - 1999 Ivar, Gary Henderson and John Weidman
+
+
+  SH assembler code partly based on x86 assembler code
+  (c) Copyright 2002 - 2004 Marcus Comstedt (marcus@mc.pp.se) 
+
+ 
+  Specific ports contains the works of other authors. See headers in
+  individual files.
+ 
+  Snes9x homepage: http://www.snes9x.com
+ 
+  Permission to use, copy, modify and distribute Snes9x in both binary and
+  source form, for non-commercial purposes, is hereby granted without fee,
+  providing that this license information and copyright notice appear with
+  all copies and any derived work.
+ 
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event shall the authors be held liable for any damages
+  arising from the use of this software.
+ 
+  Snes9x is freeware for PERSONAL USE only. Commercial users should
+  seek permission of the copyright holders first. Commercial use includes
+  charging money for Snes9x or software derived from Snes9x.
+ 
+  The copyright holders request that bug fixes and improvements to the code
+  should be forwarded to them so everyone can benefit from the modifications
+  in future versions.
+ 
+  Super NES and Super Nintendo Entertainment System are trademarks of
+  Nintendo Co., Limited and its subsidiary companies.
+*******************************************************************************/
+
+#ifndef _GETSET_H_
+#define _GETSET_H_
+
+#include "ppu.h"
+#include "dsp1.h"
+#include "cpuexec.h"
+#include "sa1.h"
+#include "c4.h"
+#include "bsx.h"
+#include "spc7110.h"
+#include "hwregisters.h"
+#include "obc1.h"
+
+INLINE uint8 S9xGetByte (uint32 Address)
 {
-	uint8	DB;
-	pair	P;
-	pair	A;
-	pair	D;
-	pair	S;
-	pair	X;
-	pair	Y;
-	PC_t	PC;
-};
+	int block;
+    uint8 *GetAddress = CPU.MemoryMap [block = (Address >> MEMMAP_SHIFT) & MEMMAP_MASK];
+#ifdef VAR_CYCLES
+	if ((intptr_t) GetAddress != Memory.MAP_CPU || !CPU.InDMA)
+        CPU.Cycles += CPU.MemorySpeed [block];
+#endif
 
-struct SSA1 {
-    struct  SOpcodes *S9xOpcodes;
-    uint8   _Carry;
-    uint8   _Zero;
-    uint8   _Negative;
-    uint8   _Overflow;
-    bool8   CPUExecuting;
-    uint32  ShiftedPB;
-    uint32  ShiftedDB;
-    uint32  Flags;
-    bool8   Executing;
-    bool8   NMIActive;
-    bool8   IRQActive;
-    bool8   WaitingForInterrupt;
-    bool8   Waiting;
-//    uint8   WhichEvent;
-    uint8   *PC;
-    uint8   *PCBase;
-    uint8   *BWRAM;
-    uint8   *PCAtOpcodeStart;
-    uint8   *WaitAddress;
-    uint32  WaitCounter;
-    uint8   *WaitByteAddress1;
-    uint8   *WaitByteAddress2;
-//    long    Cycles;
-//    long    NextEvent;
-//    long    V_Counter;
-    int16   op1;
-    int16   op2;
-    int     arithmetic_op;
-    int64   sum;
-    bool8   overflow;
-    uint8   VirtualBitmapFormat;
-    bool8   in_char_dma;
-    uint8   variable_bit_pos;
-    uint8   *Map [MEMMAP_NUM_BLOCKS];
-    uint8   *WriteMap [MEMMAP_NUM_BLOCKS];
-};
-
-#define SA1CheckZero() (SA1._Zero == 0)
-#define SA1CheckCarry() (SA1._Carry)
-#define SA1CheckIRQ() (SA1Registers.PL & IRQ)
-#define SA1CheckDecimal() (SA1Registers.PL & Decimal)
-#define SA1CheckIndex() (SA1Registers.PL & IndexFlag)
-#define SA1CheckMemory() (SA1Registers.PL & MemoryFlag)
-#define SA1CheckOverflow() (SA1._Overflow)
-#define SA1CheckNegative() (SA1._Negative & 0x80)
-#define SA1CheckEmulation() (SA1Registers.P.W & Emulation)
-
-#define SA1ClearFlags(f) (SA1Registers.P.W &= ~(f))
-#define SA1SetFlags(f)   (SA1Registers.P.W |=  (f))
-#define SA1CheckFlag(f)  (SA1Registers.PL & (f))
-
-
-START_EXTERN_C
-extern struct SSA1Registers	SA1Registers;
-extern struct SSA1			SA1;
-
-uint8 S9xSA1GetByte (uint32);
-uint8 S9xSA1GetByteSlow (uint32 address, int GetAddress);
-uint16 S9xSA1GetWord (uint32);
-uint16 S9xSA1GetWordFast (uint32);
-void S9xSA1SetByte (uint8, uint32);
-void S9xSA1SetByteSlow (uint8 byte, uint32 address, int Setaddress);
-void S9xSA1SetWord (uint16, uint32);
-void S9xSA1SetWordFast (uint16, uint32);
-void S9xSA1SetPCBase (uint32);
-uint8 S9xGetSA1 (uint32);
-void S9xSetSA1 (uint8, uint32);
-END_EXTERN_C
-
-extern struct SOpcodes S9xSA1OpcodesM1X1 [256];
-extern struct SOpcodes S9xSA1OpcodesM1X0 [256];
-extern struct SOpcodes S9xSA1OpcodesM0X1 [256];
-extern struct SOpcodes S9xSA1OpcodesM0X0 [256];
-
-//void S9xSA1MainLoop ();
-void S9xSA1CheckIRQ ();
-void S9xSA1Init ();
-void S9xFixSA1AfterSnapshotLoad ();
-//void S9xSA1ExecuteDuringSleep ();
-
-INLINE uint8 S9xSA1GetByteFast (uint32 address)
-{
-    uint8 *GetAddress = SA1.Map [(address >> MEMMAP_SHIFT) & MEMMAP_MASK];
-    // Hot path: direct memory via pointer. Slow path is the register /
-    // BWRAM-bitmap dispatch in S9xSA1GetByteSlow. Direct-memory reads
-    // dominate the SA1 opcode stream, so mark the fast side LIKELY for
-    // correct code layout.
+    // Hot path: direct memory (ROM/RAM/SRAM/etc.) via pointer.
+    // Slow path: hardware register dispatch. Registers are touched rarely
+    // compared to code fetches and data loads, so mark the fast side LIKELY
+    // to give the compiler a correct code-layout hint.
     if (LIKELY(GetAddress >= (uint8 *) CMemory::MAP_LAST))
-	    return (*(GetAddress + (address & 0xffff)));
-    else
-        return S9xSA1GetByteSlow(address, (int)GetAddress);
+		return (*(GetAddress + (Address & 0xffff)));
+	else 
+		return S9xGetByteFromRegister(GetAddress, Address);
 }
 
-INLINE void S9xSA1SetByteFast (uint8 byte, uint32 address)
+INLINE uint16 S9xGetWord (uint32 Address)
 {
-    uint8 *Setaddress = SA1.WriteMap [(address >> MEMMAP_SHIFT) & MEMMAP_MASK];
+    if (LIKELY((Address & 0x0fff) != 0x0fff))
+	{
+   		int block;
+   		uint8 *GetAddress = CPU.MemoryMap [block = (Address >> MEMMAP_SHIFT) & MEMMAP_MASK];
+				
+#ifdef VAR_CYCLES
+		if ((intptr_t) GetAddress != Memory.MAP_CPU || !CPU.InDMA)
+  	  	    CPU.Cycles += CPU.MemorySpeed [block] << 1;
+#endif
 
-    // See S9xSA1GetByteFast: direct memory is the hot path, register
-    // dispatch via S9xSA1SetByteSlow is the cold path.
-    if (LIKELY(Setaddress >= (uint8 *) CMemory::MAP_LAST))
-    {
-        *(Setaddress + (address & 0xffff)) = byte;
-        return;
-    }
-
-    S9xSA1SetByteSlow (byte, address, (int)Setaddress);
-}
-
-inline uint16 S9xSA1GetWordFast (uint32 address)
-{
-    return (S9xSA1GetByteFast (address) | (S9xSA1GetByteFast (address + 1) << 8));
-}
-
-inline void  S9xSA1SetWordFast (uint16 Word, uint32 address)
-{
-    S9xSA1SetByteFast ((uint8) Word, address);
-    S9xSA1SetByteFast ((uint8) (Word >> 8), address + 1);
-}
-
-#define SNES_IRQ_SOURCE	    (1 << 7)
-#define TIMER_IRQ_SOURCE    (1 << 6)
-#define DMA_IRQ_SOURCE	    (1 << 5)
-
-STATIC inline void S9xSA1UnpackStatus()
-{
-    SA1._Zero = (SA1Registers.PL & Zero) == 0;
-    SA1._Negative = (SA1Registers.PL & Negative);
-    SA1._Carry = (SA1Registers.PL & Carry);
-    SA1._Overflow = (SA1Registers.PL & Overflow) >> 6;
-}
-
-STATIC inline void S9xSA1PackStatus()
-{
-    SA1Registers.PL &= ~(Zero | Negative | Carry | Overflow);
-    SA1Registers.PL |= SA1._Carry | ((SA1._Zero == 0) << 1) |
-		       (SA1._Negative & 0x80) | (SA1._Overflow << 6);
-}
-
-STATIC inline void S9xSA1FixCycles ()
-{
-    if (SA1CheckEmulation ())
-	SA1.S9xOpcodes = S9xSA1OpcodesM1X1;
-    else
-    if (SA1CheckMemory ())
-    {
-	if (SA1CheckIndex ())
-	    SA1.S9xOpcodes = S9xSA1OpcodesM1X1;
+		// Hot path: contiguous word read via direct pointer.
+		// Slow path: register dispatch. See S9xGetByte for the rationale.
+		if (LIKELY(GetAddress >= (uint8 *) CMemory::MAP_LAST))
+		{
+#ifdef CPU_SHUTDOWN
+			if (Memory.BlockIsRAM [block])
+				CPU.WaitAddress = CPU.PCAtOpcodeStart;
+#endif
+#ifdef FAST_LSB_WORD_ACCESS
+			return (*(uint16 *) (GetAddress + (Address & 0xffff)));
+#else
+			return (*(GetAddress + (Address & 0xffff)) | (*(GetAddress + (Address & 0xffff) + 1) << 8));
+#endif	
+   		}
+		return S9xGetWordFromRegister(GetAddress, Address);
+	}
 	else
-	    SA1.S9xOpcodes = S9xSA1OpcodesM1X0;
-    }
-    else
+ 		return (S9xGetByte (Address) | (S9xGetByte (Address + 1) << 8));
+}
+
+INLINE void S9xSetByte (uint8 Byte, uint32 Address)
+{
+#if defined(CPU_SHUTDOWN)
+    CPU.WaitAddress = NULL;
+#endif
+    int block;
+    uint8 *SetAddress = CPU.MemoryWriteMap [block = ((Address >> MEMMAP_SHIFT) & MEMMAP_MASK)];
+	
+#ifdef VAR_CYCLES
+	if ((intptr_t) SetAddress != Memory.MAP_CPU || !CPU.InDMA)
+		CPU.Cycles += CPU.MemorySpeed [block];
+#endif
+	
+    if (LIKELY(SetAddress >= (uint8 *) CMemory::MAP_LAST))
     {
-	if (SA1CheckIndex ())
-	    SA1.S9xOpcodes = S9xSA1OpcodesM0X1;
-	else
-	    SA1.S9xOpcodes = S9xSA1OpcodesM0X0;
+		*(SetAddress + (Address & 0xffff)) = Byte;
+#ifdef CPU_SHUTDOWN
+		// Gate the SA1 wake-up detection on WaitByteAddress1 being set.
+		// For non-SA1 games these pointers are NULL, so the whole block
+		// collapses to a single predicted-not-taken branch. For SA1 games
+		// the vast majority of writes also miss this path, so the branch
+		// is kept cold. The computed address is cached in a local to avoid
+		// recomputing (SetAddress + (Address & 0xffff)) twice.
+		if (UNLIKELY(SA1.WaitByteAddress1 != NULL))
+		{
+			uint8 *writeAddr = SetAddress + (Address & 0xffff);
+			if (writeAddr == SA1.WaitByteAddress1 ||
+				writeAddr == SA1.WaitByteAddress2)
+			{
+				if (!SA1.Executing)
+					SA1.Executing = !SA1.Waiting && SA1.S9xOpcodes != NULL;
+				if (SA1.Executing) SA1.WaitCounter = 3;
+			}
+		}
+#endif
+		return;
+    }
+	S9xSetByteToRegister(Byte, SetAddress, Address);
+}
+
+INLINE void S9xSetWord(uint16 Word, uint32 Address)
+{
+#ifdef VAR_CYCLES
+	if((Address & 0x0FFF)==0x0FFF)
+	{
+		S9xSetByte(Word&0x00FF, Address);
+		S9xSetByte(Word>>8, Address+1);
+		return;
+	}
+#endif
+	
+#if defined(CPU_SHUTDOWN)
+    CPU.WaitAddress = NULL;
+#endif
+
+    int block;
+    uint8 *SetAddress = CPU.MemoryWriteMap [block = ((Address >> MEMMAP_SHIFT) & MEMMAP_MASK)];
+	
+#ifdef VAR_CYCLES
+	if ((intptr_t) SetAddress != Memory.MAP_CPU || !CPU.InDMA)
+		CPU.Cycles += CPU.MemorySpeed [block] << 1;
+#endif
+
+	if (LIKELY(SetAddress >= (uint8 *) CMemory::MAP_LAST))
+	{
+#ifdef CPU_SHUTDOWN
+		// Same optimization as S9xSetByte: gate on WaitByteAddress1 being set
+		// (NULL for non-SA1 games and for SA1 games without a registered
+		// wake-up address), and compute the write address once into a local.
+		if (UNLIKELY(SA1.WaitByteAddress1 != NULL))
+		{
+			uint8 *writeAddr = SetAddress + (Address & 0xffff);
+			if (writeAddr == SA1.WaitByteAddress1 ||
+				writeAddr == SA1.WaitByteAddress2)
+			{
+				if (!SA1.Executing)
+					SA1.Executing = !SA1.Waiting && SA1.S9xOpcodes != NULL;
+				if (SA1.Executing) SA1.WaitCounter = 3;
+			}
+		}
+#ifdef FAST_LSB_WORD_ACCESS
+	*(uint16 *) SetAddress = Word;
+#else
+	*(SetAddress + (Address & 0xffff)) = (uint8) Word;
+	*(SetAddress + ((Address + 1) & 0xffff)) = Word >> 8;
+#endif
+#else
+#ifdef FAST_LSB_WORD_ACCESS
+	*(uint16 *) (SetAddress + (Address & 0xffff)) = Word;
+#else
+	*(SetAddress + (Address & 0xffff)) = (uint8) Word;
+	*(SetAddress + ((Address + 1) & 0xffff)) = Word >> 8;
+#endif
+#endif
+	return;
+    }		
+	S9xSetWordToRegister(Word, SetAddress, Address);
+}
+
+INLINE uint8 *GetBasePointer (uint32 Address)
+{
+    uint8 *GetAddress = Map [(Address >> MEMMAP_SHIFT) & MEMMAP_MASK];
+    if (GetAddress >= (uint8 *) CMemory::MAP_LAST)
+	return (GetAddress);
+
+    switch ((int) GetAddress)
+    {
+	case CMemory::MAP_SPC7110_ROM:
+		return (S9xGetBasePointerSPC7110(Address));
+    case CMemory::MAP_PPU:
+		return (ROM_GLOBAL);
+    case CMemory::MAP_CPU:
+		return (ROM_GLOBAL);
+    case CMemory::MAP_DSP:
+		return (ROM_GLOBAL - 0x6000);
+    case CMemory::MAP_SA1RAM:
+    case CMemory::MAP_LOROM_SRAM:
+        return (SRAM);
+    case CMemory::MAP_BWRAM:
+	return (BWRAM - 0x6000);
+    case CMemory::MAP_HIROM_SRAM:
+        return (SRAM - 0x6000);
+    case CMemory::MAP_C4:
+		return (S9xGetBasePointerC4(Address & 0xffff));
+    case CMemory::MAP_DEBUG:
+#ifdef DEBUGGER
+	printf ("GBP %06x\n", Address);
+#endif
+
+    default:
+    case CMemory::MAP_NONE:
+#ifdef DEBUGGER
+	printf ("GBP %06x\n", Address);
+#endif
+	return (0);
     }
 }
+
+INLINE uint8 *S9xGetMemPointer (uint32 Address)
+{
+    uint8 *GetAddress = Map [(Address >> MEMMAP_SHIFT) & MEMMAP_MASK];
+    if (GetAddress >= (uint8 *) CMemory::MAP_LAST)
+	return (GetAddress + (Address & 0xffff));
+
+    switch ((int) GetAddress)
+    {
+	case CMemory::MAP_SPC7110_ROM:
+		return (S9xGetBasePointerSPC7110(Address) + (Address & 0xffff));
+    case CMemory::MAP_PPU:
+		return (ROM_GLOBAL + (Address & 0xffff));
+    case CMemory::MAP_CPU:
+		return (ROM_GLOBAL + (Address & 0xffff));
+    case CMemory::MAP_DSP:
+		return (ROM_GLOBAL - 0x6000 + (Address & 0xffff));
+    case CMemory::MAP_SA1RAM:
+    case CMemory::MAP_LOROM_SRAM:
+        return (SRAM + (Address & 0xffff));
+    case CMemory::MAP_BWRAM:
+		return (BWRAM - 0x6000 + (Address & 0xffff));
+    case CMemory::MAP_HIROM_SRAM:
+        return (SRAM - 0x6000 + (Address & 0xffff));
+    case CMemory::MAP_C4:
+	return (S9xGetMemPointerC4(Address & 0xffff));
+    case CMemory::MAP_DEBUG:
+#ifdef DEBUGGER
+	printf ("GMP %06x\n", Address);
+#endif
+    default:
+    case CMemory::MAP_NONE:
+#ifdef DEBUGGER
+	printf ("GMP %06x\n", Address);
+#endif
+	return (0);
+    }
+}
+
+INLINE void S9xSetPCBase (uint32 Address)
+{
+    int block;
+    uint8 *GetAddress = Map [block = (Address >> MEMMAP_SHIFT) & MEMMAP_MASK];
+
+	CPU.MemSpeed = Memory.MemorySpeed [block];
+	CPU.MemSpeedx2 = CPU.MemSpeed << 1;
+ 
+   if (GetAddress >= (uint8 *) CMemory::MAP_LAST)
+    {
+		CPU.PCBase = GetAddress;
+		CPU.PC = GetAddress + (Address & 0xffff);
+		return;
+    }
+    S9xSetPCBaseOthers(GetAddress, Address);
+}
+
 #endif
